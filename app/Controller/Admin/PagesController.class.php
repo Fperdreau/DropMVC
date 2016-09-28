183da4985c3593e53a4670d747803a1227bcd64e
Florian Perdreau <f.perdreau@donders.ru.nl>
2016-09-28 07:56:39 +0200

    changed files header

--- app/Controller/Admin/PagesController.class.php
+++ app/Controller/Admin/PagesController.class.php
@@ -6,20 +6,20 @@
  * @copyright Copyright (C) 2016 Florian Perdreau
  * @license <http://www.gnu.org/licenses/agpl-3.0.txt> GNU Affero General Public License v3
  *
- * This file is part of JR-Haarsieraad.
+ * This file is part of DropMVC.
  *
- * JR-Haarsieraad is free software: you can redistribute it and/or modify
+ * DropMVC is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JR-Haarsieraad is distributed in the hope that it will be useful,
+ * DropMVC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with JR-Haarsieraad.  If not, see <http://www.gnu.org/licenses/>.
+ * along with DropMVC.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace App\Controller\Admin;
